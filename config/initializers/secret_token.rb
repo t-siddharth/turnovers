bd7ea1bf2a1f24c8d3f3f96c73ca1f06a40687d9
siddtewari <sidtewari@gmail.com>
2013-04-09 22:57:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcards::Application.config.secret_token = '2b30eb1e171ff5c103b9aad38c6c9ad29dcf0f1f429e78668a4eec4b764f3f5cf99caff051da8b32341c5645413c5d0a318e75be4806e38aeb0fc57d590c27b9'
